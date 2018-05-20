7ae43dcd6cd67668b13255401061adf08fb98441
Daniel Gil <danigilchu@gmail.com>
2018-05-20 19:09:39 +0200

    hola

--- Prueba_movimiento_teclado.py
+++ Prueba_movimiento_teclado.py
@@ -1,72 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 11:45:24 2018
+
+@author: Ignacio Paillalef, Ali mehdi, Daniel Gil
+"""
 from tkinter import *
 from random import randrange
+# ========== GLOBAL variables ==========
+Cx, Cy = 500, 500 # Dimensions Canvas
 
-global x,y,x1,x2,coord
-x,y,x1,x2=10,10,100,100
-coord = [x, y, x1, x2]
+x1, y1 = Cx//2, Cy//2 # Position intiale
+dx, dy = 0, 10 # Vitesse
 
+flag = False
+
+
+# ========== fonctions ==========
+
+# Mouvement constant
+def moveConst():
+    global x1, y1, dx, dy, flag
+    x1, y1 = x1 + dx, y1 + dy
+    # Passe a l'autre cote
+    if x1 > Cx:
+        x1 = 0
+    if y1 > Cy:
+        y1 = 0
+    if x1 < 0:
+        x1 = Cx
+    if y1 < 0:
+        y1 = Cy
+    can.coords(snake, x1, y1, x1 + 30, y1 + 30)
+    if flag:
+        fen.after(100, moveConst)
+# Arreter mouvement constant
+def stop():
+    global flag
+    flag = False
+# Demarrer mouvement constant
+def start():
+    global flag
+    if not flag:
+        flag = True
+        moveConst()
+        creation_pomme()
+# Mouvement Joueur
+def up(*ignore):
+    global dx, dy
+    if dy >= 0:
+        dx, dy = 0, -10
+def down(*ignore):
+    global dx, dy
+    if dy <= 0:
+        dx, dy = 0, 10
+def left(*ignore):
+    global dx, dy
+    if dx >= 0:
+        dx, dy = -10, 0
+def right(*ignore):
+    global dx, dy
+    if dx <= 0:
+        dx, dy = 10, 0
+# Nourriture et le fait de manger
 def creation_pomme():
     rayon=10
-    global posx, posy
-    posx=randrange(10,690,1)
-    posy=randrange(10,690,1)
-    pomme=canvas1.create_oval(posx-rayon,posy-rayon,posx+rayon,posy+rayon,
+    global posx, posy,x1
+    posx=randrange(10,490,1)
+    posy=randrange(10,490,1)
+    pomme=can.create_oval(posx-rayon,posy-rayon,posx+rayon,posy+rayon,
                           fill='red')
-
-    
-def mouvement(event):
-        if event.keysym == 'Up':
-            canvas1.move(rect, 0,-5)
-        if event.keysym == 'Up' and 'Right':
-            canvas1.move(rect, 5,-5)
-        if event.keysym == 'Up' and 'Left':
-            canvas1.move(rect, -5,-5)
-        if event.keysym == 'Down':
-            canvas1.move(rect, 0,5)
-        if event.keysym == 'Down' and 'Right':
-            canvas1.move(rect, 5,5)
-        if event.keysym == 'Down' and 'Left':
-            canvas1.move(rect, -5,5)
-        if event.keysym == 'Right':
-            canvas1.move(rect, 5,0)
-            auto1()
-            creation_pomme()
-        if event.keysym == 'Left':
-            canvas1.move(rect, -5,0)
-            #window.after(1000,mouvement)
-
-            
-def auto1():    # Mouvement automatique vers la droite
-    global rect, coord
-    coord[0]= coord[0] + 5
-    coord[2]=coord[2] + 5
-    print(coord[0],coord[2])
-    canvas1.coords(rect, coord[0], coord[1], coord[2], coord[3])
-    window.after(100, auto1)
-
-def auto2():
-    global rect, coord
-    coord[0]= coord[0] + 5
-    coord[2]=coord[2] + 5
-    print(coord[0],coord[2])
-    canvas1.coords(rect, coord[0], coord[1], coord[2], coord[3])
-    window.after(100, auto)
-
-
-window = Tk()
-window.geometry("800x800")
-
-#canvas et dessin (rectangle)
-canvas1=Canvas(window, height = 700, width = 700)
-canvas1.grid(row=0, column=0, sticky=W)
-coord = [x, y, x1, x2]
-rect = canvas1.create_rectangle(*coord, outline="aquamarine", fill="cyan")
+    if(posx+10 == x1 or posx-10 == x1 or posy+10 == x1 or posy-10 == x1 ):
+        can.delete(pomme)
+        creation_pomme()
 
 
 
-#Associer les touches à la fonction
-window.bind_all('<Up>', mouvement)
-window.bind_all('<Down>', mouvement)
-window.bind_all('<Left>', mouvement)
-window.bind_all('<Right>', mouvement)
-window.mainloop()
+# ========== MAIN PROGRAM ==========
+
+
+fen = Tk()
+fen.title("Snake")
+
+can = Canvas(fen, bg = 'dark grey', height = Cx, width = Cy)
+can.pack(side = LEFT, padx = 5, pady = 5)
+
+# SNAKE
+snake = can.create_rectangle(x1, y1, x1 + 30, y1 + 30, width = 2, fill = 'red')
+fen.bind("<Up>", up)            # Bouttons déplacement
+fen.bind("<Down>", down)
+fen.bind("<Left>", left)
+fen.bind("<Right>", right)
+
+# Boutons
+btn1 = Button(fen, text='Start', command = start)     # Commencer mouvement
+btn1.pack()
+
+btn2 = Button(fen, text='Stop', command = stop)     # Arreter mouvement
+btn2.pack()
+
+# TODO: Pour quoi ça?
+can.focus_set()
+
+fen.mainloop()
